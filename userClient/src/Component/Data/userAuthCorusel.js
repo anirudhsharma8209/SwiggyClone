f724c485b39df23208f3686a870355ccd067d3ba
anirudhsharma8209 <devprakashsharma7790@gmail.com>
2024-04-01 16:46:45 +0530

    Adding require data

---
+++ userClient/src/Component/Data/userAuthCorusel.js
@@ -0,0 +1,17 @@
+export const crousel = [
+    {
+        id : 1,
+        url : "https://images.unsplash.com/photo-1552526881-721ce8509abb?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name : "burger"        
+    },
+    {
+        id : 2,
+        url : "https://images.unsplash.com/photo-1598214887283-1b96ceceea96?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name : "pancake"
+    },
+    {
+        id : 3,
+        url : "https://plus.unsplash.com/premium_photo-1673590981810-894dadc93a6d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name : "vigies"
+    }
+]

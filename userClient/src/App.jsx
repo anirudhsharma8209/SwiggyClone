965db2b9d2da99bf2b4f4351225a37ebd76488e5
anirudhsharma8209 <devprakashsharma7790@gmail.com>
2024-04-01 16:50:50 +0530

    Adding updated files

--- userClient/src/App.jsx
+++ userClient/src/App.jsx
@@ -1,14 +1,24 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Navigation from './Component/Navbar/Navigation'
 import Header from './Component/Header/Header'
+import UserRegistration from './Component/UserAuthoncation/UserRegistration'
+import SubMenu from './Component/SubMenu/SubMenu'
+import About from './Component/About/About'
 const App = () => {
+  const [navbar, setNavbar] = useState(true);
   return (
     <Fragment>
         <BrowserRouter>
-            <Navigation />
-            <Routes>
-                <Route element={<Header />} path='/' />
+        {
+          console.log(navbar)
+        }
+            {navbar == true && <Navigation setNavbar={setNavbar} /> }
+            <Routes>                
+                {['home', '/'].map(path => <Route element={<Header />} path={path}  />)}
+                <Route element={<SubMenu />} path='/submenu' />
+                <Route element={<UserRegistration setNavbar={setNavbar} />} path='registration' />
+                <Route element={<About />} path='/about' />
             </Routes>
         </BrowserRouter>
     </Fragment>
